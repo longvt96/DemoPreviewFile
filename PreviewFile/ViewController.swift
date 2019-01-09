8622f2c2b72c100cbd7814353218ff1117dec8ff
vu.thanh.long <vu.thanh.long@framgias-mac-mini.local>
2019-01-09 09:29:35 +0700

    Initial Commit

---
+++ PreviewFile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PreviewFile
+//
+//  Created by vu.thanh.long on 1/9/19.
+//  Copyright © 2019 vu.thanh.long. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
